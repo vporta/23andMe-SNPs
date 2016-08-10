43e53116b6431c5d3914b3fb6c8d970a4f3a3fc7
vporta <vporta7@gmail.com>
2016-08-10 16:02:43 -0400

    created first library

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+var uniqueRandomArray = require('unique-random-array');
 var snps23andMe = require ('./snps.json');
 
+
 module.exports = {
-    all: snps23andMe 
+    all: snps23andMe,
+    random: uniqueRandomArray(snps23andMe)
 };
